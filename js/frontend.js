3f89ba537fcfd98e74cb3ea62b06e12fbbd33672
Dan Smith <dev@dotherightthing.co.nz>
2018-05-20 23:43:08 +1200

    Lint with JSLint

--- js/frontend.js
+++ js/frontend.js
@@ -3,17 +3,21 @@
  *
  * PHP variables are provided in wpdtrt_blocks_config.
  *
- * @version 	1.0.0
- * @since       0.7.6
+ * @version 1.0.0
+ * @since   0.7.6
  */
 
-jQuery(document).ready(function($){
+/*global document, $, jQuery, wpdtrt_blocks_config*/
 
-	var config = wpdtrt_blocks_config;
+jQuery(document).ready(function ($) {
 
-	$('.wpdtrt-blocks-badge').hover(function() {
-		$(this).find('.wpdtrt-blocks-badge-info').stop(true, true).fadeIn(200);
-	}, function() {
-		$(this).find('.wpdtrt-blocks-badge-info').stop(true, true).fadeOut(200);
-	});
+    "use strict";
+
+    // var config = wpdtrt_blocks_config;
+
+    $('.wpdtrt-blocks-badge').hover(function () {
+        $(this).find('.wpdtrt-blocks-badge-info').stop(true, true).fadeIn(200);
+    }, function () {
+        $(this).find('.wpdtrt-blocks-badge-info').stop(true, true).fadeOut(200);
+    });
 });

2c2deca07c41789aac794b4cfc5bbf824a0696fc
Dan Smith <dev@dotherightthing.co.nz>
2018-05-22 13:13:12 +1200

    Bump version

--- gulpfile.js
+++ gulpfile.js
@@ -8,7 +8,7 @@
  *    gulp dist --gulpfile ./vendor/dotherightthing/wpdtrt-plugin/gulpfile.js --cwd ./
  *
  * @package     WPDTRT_Blocks
- * @version 	1.1.4
+ * @version 	1.1.5
  * @since       0.7.6
  */
  
--- src/class-wpdtrt-blocks-plugin.php
+++ src/class-wpdtrt-blocks-plugin.php
@@ -12,7 +12,7 @@
  *
  * @since   1.0.0
  */
-class WPDTRT_Blocks_Plugin extends DoTheRightThing\WPDTRT_Plugin_Boilerplate\r_1_4_21\Plugin {
+class WPDTRT_Blocks_Plugin extends DoTheRightThing\WPDTRT_Plugin_Boilerplate\r_1_4_22\Plugin {
 
 	/**
 	 * Supplement plugin initialisation.

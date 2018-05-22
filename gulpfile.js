7321afa964a129c3c093cd97dc1259b5d6370882
Dan Smith <dev@dotherightthing.co.nz>
2018-05-22 14:02:30 +1200

    Fix boilerplate name

--- gulpfile.js
+++ gulpfile.js
@@ -3,9 +3,9 @@
  * Compile front-end resources
  *
  * @example usage from child plugin:
- *    gulp --gulpfile ./vendor/dotherightthing/wpdtrt-plugin/gulpfile.js --cwd ./
- *    gulp dev --gulpfile ./vendor/dotherightthing/wpdtrt-plugin/gulpfile.js --cwd ./
- *    gulp dist --gulpfile ./vendor/dotherightthing/wpdtrt-plugin/gulpfile.js --cwd ./
+ *    gulp --gulpfile ./vendor/dotherightthing/wpdtrt-plugin-boilerplate/gulpfile.js --cwd ./
+ *    gulp dev --gulpfile ./vendor/dotherightthing/wpdtrt-plugin-boilerplate/gulpfile.js --cwd ./
+ *    gulp dist --gulpfile ./vendor/dotherightthing/wpdtrt-plugin-boilerplate/gulpfile.js --cwd ./
  *
  * @package     WPDTRT_Blocks
  * @version 	1.1.5
